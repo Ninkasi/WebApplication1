b24480c99203decae9a3673fd3e2661d6662be6e
Juha Saarijrvi <juha.saarijarvi@gmail.com>
2019-03-29 00:56:47 +0200

    Add project files.

---
+++ WebApplication1/Base64Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public static class Base64Dekooderi
+    {
+        public static string EnkoodaaTeksti(string teksti)
+        {
+            byte[] puskuri = System.Text.Encoding.UTF8.GetBytes(teksti);
+           
+            return Convert.ToBase64String(puskuri);
+        }
+
+        public static string DekoodaaTeksti(string teksti)
+        {
+            byte[] puskuri = Convert.FromBase64String(teksti);
+            
+            return System.Text.Encoding.UTF8.GetString(puskuri);
+        }
+    }
+}
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Base64Dekooderi dekooderi = new Base64Dekooderi();
+
+            alkuTxt.Text = dekooderi.MyMessage;
+
+            if(tbEka.Text.Length > 0)
+            {
+                dekooderi.MyMessage = tbEka.Text;
+            }
+
+            string temppi = dekooderi.EnkoodaaTeksti(dekooderi.MyMessage);
+
+            alkuTxtEnkoodattu.Text = temppi;
+
+            alkuTxtDekoodattu.Text = dekooderi.DekoodaaTeksti(temppi);
+           
+        }
+    }
+}
